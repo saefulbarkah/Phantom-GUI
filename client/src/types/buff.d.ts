c4cfb55b0e7f8f31467843d795fac3fa14d2082d
XopH <saefulbarkah650@gmail.com>
2025-08-12 09:53:19 +0700

    feat(types): add buff types for selected buffs and buffs

---
+++ client/src/types/buff.d.ts
@@ -0,0 +1,11 @@
+export type TSelectedBuff = {
+  id: number | number[] | null;
+  name: string | null;
+  stacks: number | null;
+};
+
+export type TBuffs = {
+  id: number | number[];
+  name: string;
+  stacks: number;
+};
