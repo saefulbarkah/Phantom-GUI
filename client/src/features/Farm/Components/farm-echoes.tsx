131dbdc6308f39969b7e0540dcd23149575c0b52
XopH <saefulbarkah650@gmail.com>
2025-08-15 09:28:07 +0700

    feat(UI): farm info

--- client/src/features/Farm/Components/farm-echoes.tsx
+++ client/src/features/Farm/Components/farm-echoes.tsx
@@ -6,7 +6,9 @@ import { LoadingContent } from "@/components/LoadingContent";
 import { Button } from "@/components/ui/button";
 import { useFarms, useFarmState } from "@/hooks/useFarms";
 import { useFeatureManager } from "@/hooks/useFeatureManager";
+import { TSonataList } from "@/types/farm";
 import { RefreshCcw } from "lucide-react";
+import Image from "next/image";
 import React, { useEffect, useMemo, useState } from "react";
 import toast from "react-hot-toast";
 
@@ -16,9 +18,22 @@ const byCost: TOptionValue[] = [
   { label: "1", value: "1" },
 ];
 
+type NullablePartial<T> = {
+  [P in keyof T]?: T[P] | null;
+};
+
 export const FarmEchoes = () => {
   const { filter, setFilter, monsters, setMonster } = useFarmState();
   const [SonataValue, setSonataValue] = useState("");
+
+  const [sonata, setSonata] = useState<NullablePartial<TSonataList>>({
+    icon: null,
+    id: null,
+    monsters: [],
+    name: null,
+    sonataEffects: [],
+  });
+
   const [CostValue, setCostValue] = useState("");
   const [EchoValue, setEchoValue] = useState("");
 
@@ -45,6 +60,7 @@ export const FarmEchoes = () => {
         value: item.name,
         real_value: item.id,
         icon: `./assets/icon/sonata/${item.icon.toLowerCase()}.webp`,
+        sonataEffects: item.sonataEffects,
         monsters: item.monsters,
       })) ?? []
     );
@@ -102,7 +118,7 @@ export const FarmEchoes = () => {
   return (
     <div className="flex flex-col gap-5">
       <h2 className="text-xl font-semibold">Auto Farm Echoes</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 items-start">
         <FeatureCardSwitch
           title="Auto Farm Option"
           description="Automatically farm echoes and auto kill target monster detection"
@@ -118,6 +134,7 @@ export const FarmEchoes = () => {
                 setFilter({ bySonataId: data.real_value });
                 setMonster(data.monsters);
                 setEchoValue("All");
+                setSonata({ name: data.label, icon: data.icon, sonataEffects: data.sonataEffects });
               }}
             />
 
@@ -156,6 +173,59 @@ export const FarmEchoes = () => {
             </Button>
           </div>
         </FeatureCardSwitch>
+
+        <div className="col-span-2">
+          <FeatureCardSwitch
+            title="Farm Information"
+            description="Auto farm information detail about monster detection and target sonata"
+          >
+            <div className="w-full md:w-fit divide-y divide-gray-700">
+              <p className="py-2">Target detection</p>
+              <div className="grid grid-cols-[1fr_auto] md:grid-cols-[150px_250px] items-center gap-4 py-2">
+                <span className="text-gray-300">Sonata</span>
+                <div className="flex gap-2 items-center">
+                  {sonata.name ? (
+                    <>
+                      <Image alt="" src="./assets/icon/sonata/havoc-eclipse.webp" width={20} height={20} />
+                      <span>{SonataValue}</span>
+                    </>
+                  ) : (
+                    <span>-</span>
+                  )}
+                </div>
+              </div>
+
+              <div className="grid grid-cols-[1fr_auto] md:grid-cols-[150px_auto] items-center gap-4 py-2">
+                <span className="text-gray-300">Cost</span>
+                {CostValue !== "" ? <span>{CostValue}</span> : <span>-</span>}
+              </div>
+
+              <div className="grid grid-cols-[1fr_auto] md:grid-cols-[150px_auto] items-center gap-4 py-2">
+                <span className="text-gray-300">Echo</span>
+                {EchoValue !== "" ? <span>{EchoValue}</span> : <span>-</span>}
+              </div>
+            </div>
+
+            <h2 className="mt-5 mb-2">Sonata Effects</h2>
+            <div className="flex flex-col divide-y divide-gray-700">
+              <div className="flex flex-col gap-2 py-3">
+                <p>2-piece effect</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eius temporibus accusamus minima aperiam
+                  nisi, quibusdam id nesciunt eum veritatis?
+                </p>
+              </div>
+
+              <div className="flex flex-col gap-2 py-3">
+                <p>4-piece effect</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui eius temporibus accusamus minima aperiam
+                  nisi, quibusdam id nesciunt eum veritatis?
+                </p>
+              </div>
+            </div>
+          </FeatureCardSwitch>
+        </div>
       </div>
     </div>
   );
