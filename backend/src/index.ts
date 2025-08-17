8903cdd8af8c8a585c70326f13c1c6b7454c3ee2
XopH <saefulbarkah650@gmail.com>
2025-08-17 14:04:13 +0700

    without env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,9 @@
 import express from "express";
-import dotenv from "dotenv";
 import routes from "./routes/routes";
 import cors from "cors";
 
-dotenv.config();
-
 const app = express();
-const port = process.env.PORT;
+const port = 51235;
 
 // Atur CORS supaya origin tertentu diizinkan
 app.use(
