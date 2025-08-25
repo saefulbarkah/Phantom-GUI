e91c97c7c97979ad7b0a58c88497bfd6182fac5e
XopH <saefulbarkah650@gmail.com>
2025-08-25 13:39:01 +0700

    sidecar server

--- client/src/providers/QueryProvider.tsx
+++ client/src/providers/QueryProvider.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
+import React, { useEffect } from "react";
+import { Command } from "@tauri-apps/plugin-shell";
+import { listen, TauriEvent } from "@tauri-apps/api/event";
+import toast from "react-hot-toast";
 
 export const QueryProvider = ({ children }: React.ComponentProps<"div">) => {
   const queryClient = new QueryClient({
@@ -12,5 +15,27 @@ export const QueryProvider = ({ children }: React.ComponentProps<"div">) => {
       },
     },
   });
+
+  const RunServer = async () => {
+    const t = toast.loading("Connecting to server...");
+    const command = Command.sidecar("binaries/phantom");
+
+    command.spawn().then((child) => {
+      /**
+       * Killing server process when window is closed. Probably won't
+       * work for multi window application
+       */
+      listen(TauriEvent.WINDOW_DESTROYED, function () {
+        child.kill();
+      });
+
+      toast.success("Server connected", { id: t });
+    });
+  };
+
+  useEffect(() => {
+    RunServer();
+  }, []);
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
