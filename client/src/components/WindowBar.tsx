8c80bc8c25756c4fb418db362b25e5b603679618
XopH <saefulbarkah650@gmail.com>
2025-08-25 16:58:01 +0700

    remove unusued component

--- client/src/components/WindowBar.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-import { getCurrentWindow } from "@tauri-apps/api/window";
-import { Minimize } from "lucide-react";
-
-export const WindowBar = () => {
-  return <div className="fixed inset-0 z-50 pointer-events-none" onMouseDown={(e) => {}} />;
-};
