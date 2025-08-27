2bf38d0d4080c785cc2f6c0223d445587e5cacc7
XopH <saefulbarkah650@gmail.com>
2025-08-27 12:49:45 +0700

    put version in .env

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <div className="fixed bottom-0 right-0 left-[224px] w-[calc(100%-224px)] border-t border-t-slate-400/15 bg-background shadow-2xl shadow-box h-12 px-5">
       <div className="flex items-center justify-between w-full h-full">
-        <p className="text-sm font-light text-slate-400">v0.9 Beta</p>
+        <p className="text-sm font-light text-slate-400">{process.env.NEXT_PUBLIC_VERSION}</p>
         <div className="flex items-center gap-5">
           <Link href={"https://ko-fi.com/xoph29"} target="_blank">
             <Button
